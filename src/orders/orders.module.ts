d57a5df3dedae1e640bccd99d240d7dff4d6f7ca
Henry Merchan <henry.merchan@halopowered.com>
2024-06-21 18:12:27 -0300

    orders microservice

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, ORDER_SERVICE } from 'src/config';
+import { OrdersController } from './orders.controller';
+
+@Module({
+  controllers: [OrdersController],
+  providers:[],
+  imports:[
+    ClientsModule.register([
+      {
+        name: ORDER_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.ordersMicroservicesHost,
+          port: envs.ordersMicroservicesPort,
+        },
+      },
+    ]),
+
+  ]
+})
+export class OrdersModule {}
