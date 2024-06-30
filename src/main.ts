f7cd619957898c213e481582be020f51e43e4750
Henry Merchan <henry.merchan@halopowered.com>
2024-06-30 13:39:57 -0300

    testing submodule second commit

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
 
-  console.log('Testing submodule - first change')
+  console.log('Testing submodule - second change')
 
   logger.log(`Gateway running on port ${envs.port}`);
 }
